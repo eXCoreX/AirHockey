5eabe714b9ade2b1032e6612c60e846bdf9658ed
maksloboda <maksloboda123@gmail.com>
2017-10-14 23:08:09 +0300

    Added multitouch

--- Scripts/TouchGrabber.gd
+++ Scripts/TouchGrabber.gd
@@ -1,18 +1,22 @@
-extends Area2D
+extends Control
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
+var touchID = -1;
+var entities
+
 func _ready():
-	set_process(true);
+	set_process_input(true)
+	entities = get_parent().get_node("Node2D").get_children()
 	pass
 
 func _input(event):
+	#if (event.type == InputEvent.SCREEN_TOUCH && event.pressed):
+	#	print(event.index);
+	#if (event.type == InputEvent.SCREEN_TOUCH && !event.pressed):
+	#	touchID = -1;
 	if (event.type == InputEvent.SCREEN_TOUCH):
-		get_parent().get_node("Node2D").set_pos(event.pos)
-		print(event.pos)
-
-func _process(delta):
-	
-	pass
+		#get_parent().get_node("Node2D").set_pos(event.pos)
+		entities[event.index].set_global_pos(event.pos)
