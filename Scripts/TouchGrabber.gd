cde3341b15e71b1dc4c757d6bf7485ea54560398
maksloboda <maksloboda123@gmail.com>
2017-10-15 15:05:21 +0300

    Basic control

--- Scripts/TouchGrabber.gd
+++ Scripts/TouchGrabber.gd
@@ -1,22 +1,25 @@
 extends Control
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 var touchID = -1;
 var entities
+onready var rect = get_rect()
+
+export(NodePath) var puck
 
 func _ready():
 	set_process_input(true)
 	entities = get_parent().get_node("Node2D").get_children()
+	#print(get_rect())
 	pass
 
 func _input(event):
-	#if (event.type == InputEvent.SCREEN_TOUCH && event.pressed):
-	#	print(event.index);
-	#if (event.type == InputEvent.SCREEN_TOUCH && !event.pressed):
-	#	touchID = -1;
-	if (event.type == InputEvent.SCREEN_DRAG):
-		#get_parent().get_node("Node2D").set_pos(event.pos)
-		entities[event.index].set_global_pos(event.pos)
+	if (event.type == InputEvent.SCREEN_TOUCH && event.pressed && touchID == -1 && rect.has_point(event.pos)):
+		touchID = event.index
+		get_node(puck).setGrabbed(true);
+		get_node(puck).moveTo(event.pos)
+	if (event.type == InputEvent.SCREEN_TOUCH && !event.pressed && event.index == touchID):
+		touchID = -1;
+		get_node(puck).setGrabbed(false);
+	if (event.type == InputEvent.SCREEN_DRAG && rect.has_point(event.pos) && event.index == touchID):
+		entities[touchID].set_global_pos(event.pos)
+		get_node(puck).moveTo(event.pos)
