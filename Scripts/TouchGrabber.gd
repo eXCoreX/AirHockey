4e221088171e6bf8cd84b11cdc30d33aafdf856e
maksloboda <maksloboda123@gmail.com>
2017-10-22 13:12:15 +0300

    Fixed aspect ratio bug

--- Scripts/TouchGrabber.gd
+++ Scripts/TouchGrabber.gd
@@ -2,6 +2,7 @@ extends Control
 
 var touchID = -1;
 var entities
+onready var camera = get_node("../Camera2D")
 onready var rect = get_rect()
 export(NodePath) var puck
 
@@ -12,19 +13,37 @@ func _ready():
 	pass
 
 func clampS(pos, radius, minimum, maximum):
-	if(pos - radius < minimum):
-		pos = minimum + radius
-	if(pos + radius > maximum):
-		pos = maximum - radius
+	if(pos.x - radius < minimum.x):
+		pos.x = minimum.x + radius
+	if(pos.x + radius > maximum.x):
+		pos.x = maximum.x - radius
+		
+	if(pos.y - radius < minimum.y):
+		pos.y = minimum.y + radius
+	if(pos.y + radius > maximum.y):
+		pos.y = maximum.y - radius
 	return pos
 	pass
 
 func _input(event):
-	if (event.type == InputEvent.SCREEN_TOUCH && event.pressed && touchID == -1 && rect.has_point(event.pos)):
+	#var viewport = get_viewport_rect()
+	#var correction = get_viewport_transform().get_origin()
+	#print(correction)
+	#event = make_input_local(event)
+	event = make_input_local(event)
+	#var transform = camera.get_transform().xform_inv(event.pos) + Vector2(640, 360)
+	var transform = get_transform().xform(event.pos)
+	var fpos
+	#print(transform)
+	#entities[9].set_global_pos(transform)
+	if (event.type == InputEvent.SCREEN_TOUCH && event.pressed && touchID == -1 && rect.has_point(transform)):# && rect.has_point(event.pos-correction)):
+		#print(event.pos-correction)
 		touchID = event.index
 		get_node(puck).setGrabbed(true);
-		entities[touchID].set_global_pos(event.pos)
-		get_node(puck).moveTo(event.pos)
+		fpos = transform
+		fpos = clampS(fpos, 96, rect.pos, rect.end)
+		entities[touchID].set_global_pos(fpos)
+		get_node(puck).moveTo(fpos)
 	
 	if (event.type == InputEvent.SCREEN_TOUCH && !event.pressed && event.index == touchID):
 		touchID = -1;
@@ -32,9 +51,10 @@ func _input(event):
 		
 	if (event.type == InputEvent.SCREEN_DRAG && event.index == touchID):
 		get_node(puck).setGrabbed(true);
-		var fpos = event.pos
+		fpos = transform
 		#print(rect.end)
-		fpos.x = clampS(fpos.x, 96, rect.pos.x, rect.end.x)
-		fpos.y = clampS(fpos.y, 96, rect.pos.y, rect.end.y)
+		fpos = clampS(fpos, 96, rect.pos, rect.end)
+		#entities[touchID].set_global_pos(fpos)
+		#get_node(puck).moveTo(fpos)
 		entities[touchID].set_global_pos(fpos)
 		get_node(puck).moveTo(fpos)
