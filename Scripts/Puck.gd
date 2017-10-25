76a4048295fbbc910aec78e4ea6be660dfe2bbf3
eXCoreX <excoreky@gmail.com>
2017-10-25 21:07:47 +0300

    Puck SFX added

--- Scripts/Puck.gd
+++ Scripts/Puck.gd
@@ -1,9 +1,11 @@
 extends RigidBody2D
 
 onready var TweenNode = get_node("Tween") #Tween node
+onready var SP = get_node("SamplePlayer") #SFX Player
 var counter = 0; # counter for hits
 
 const sv = 350 #start velocity
+var canPlayS = true
 var velocity = Vector2(sv, 0) #current velocity
 var maxVelocity = 3000 #Maximum velocity
 
@@ -11,6 +13,9 @@ func _ready():
 	set_linear_velocity(velocity) #start velocity
 	set_fixed_process(true) #set fixed process
 	pass
+
+func canPlayYes():
+	canPlayS = true
 
 func _integrate_forces(state):
 	if(state.get_contact_count() < 1):
@@ -21,6 +26,9 @@ func _integrate_forces(state):
 	var collideObj = state.get_contact_collider_object(0) #get colliding object
 	var newVelocity = normal.reflect(velocity) #reflect the velocity
 	if(collideObj.has_method("get_linear_velocity")): # if colliding body is rigidBody
+		if(canPlayS):
+			var id = SP.play("paddle")
+			SP.set_volume(id, 0.95)
 		var paddleVelocity = collideObj.get_linear_velocity() #get paddle velocity
 		var material = collideObj.get_paddle_material() #get paddle material
 		material.set_shader_param("flash", 1) #flash
@@ -34,9 +42,14 @@ func _integrate_forces(state):
 			TweenNode.interpolate_property(self, "velocity/linear", newVelocity, 0.85*newVelocity, 2, Tween.TRANS_LINEAR, Tween.EASE_OUT) #slowdown slowly
 			TweenNode.start()
 	else: #if object is static
+		if(canPlayS):
+			var id = SP.play("wall")
+			SP.set_volume(id, 0.80)
 		TweenNode.interpolate_property(self, "velocity/linear", newVelocity, 0.95*newVelocity, 2, Tween.TRANS_SINE, Tween.EASE_OUT) #slowdown
 		TweenNode.start()
-
+	canPlayS = false
+	TweenNode.interpolate_callback(self, 0.05, "canPlayYes")
+	TweenNode.start()	
 	newVelocity = min(newVelocity.length(), maxVelocity) * newVelocity.normalized() #clamp velocity
 	set_linear_velocity(newVelocity) #force set velocity
 	
